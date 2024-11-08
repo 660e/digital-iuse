e947110c82347946350309a455a03eb3f0131d02
Min <solunar@vip.qq.com>
2024-11-08 22:38:25 +0800

    Update

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,9 +1,33 @@
 <script setup lang="ts">
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+const scene = new THREE.Scene();
+scene.add(new THREE.GridHelper(100, 10));
+
+const camera = new THREE.PerspectiveCamera();
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+camera.position.set(100, 100, 100);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+
+const orbitControls = new OrbitControls(camera, renderer.domElement);
+const animate = () => {
+  orbitControls.update();
+  renderer.render(scene, camera);
+};
+renderer.setAnimationLoop(animate);
+
+const bodyRef = ref();
+onMounted(() => {
+  bodyRef.value.appendChild(renderer.domElement);
+});
 </script>
 
 <template>
-  <div>
+  <div ref="bodyRef">
     <div class="fixed right-2 bottom-2 text-xs leading-none">v{{ THREE.REVISION }}</div>
   </div>
 </template>
